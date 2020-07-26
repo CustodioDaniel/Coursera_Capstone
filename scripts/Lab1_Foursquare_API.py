223cfea680dc5e4ca21395f7b0bb4ad7b05bf780
CustodioD_Win10 <daniel.dc.custodio@gmail.com>
2020-07-26 19:14:19 -0300

    Foursquare Lab completed and debuged

--- scripts/Lab1_Foursquare_API.py
+++ scripts/Lab1_Foursquare_API.py
@@ -1,5 +1,6 @@
 import requests  # library to handle requests
 import pandas as pd  # library for data analsysis
+import matplotlib.pyplot as plt
 import numpy as np  # library to handle data in a vectorized manner
 import random  # library for random number generation
 from geopy.geocoders import Nominatim  # module to convert an address into latitude and longitude values
@@ -8,7 +9,7 @@ from IPython.core.display import HTML
 from pandas import json_normalize
 import folium  # plotting library
 import webbrowser  # to display the maps on default Web browser (html)
-from data.client_config import CLIENT_ID, CLIENT_SECRET
+from data.client_config import CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN
 
 print('Folium installed')
 print('Libraries imported.')
@@ -157,19 +158,20 @@ tip = results['response']['tips']['items'][0]
 tip.keys()
 
 # Format column width and display all tips
-pd.set_option('display.max_colwidth', -1)
-tips_df = json_normalize(tips) # json normalize tips
+tips_df = json_normalize(tips)  # json normalize tips
 # columns to keep
-filtered_columns = ['text', 'agreeCount', 'disagreeCount', 'id', 'user.firstName', 'user.lastName', 'user.gender', 'user.id']
+filtered_columns = ['text', 'agreeCount', 'disagreeCount', 'id', 'user.firstName', 'user.lastName', 'user.id']
 tips_filtered = tips_df.loc[:, filtered_columns]
 # display tips
 print(tips_filtered)
 
 # Search a Foursquare User
 
-user_id = '484542633' # user ID with most agree counts and complete profile
+user_id = '484542633'  # user ID with most agree counts and complete profile
+
 url = 'https://api.foursquare.com/v2/users/' \
-      '{}?client_id={}&client_secret={}&v={}'.format(user_id, CLIENT_ID, CLIENT_SECRET, VERSION)  # define URL
+      '{}?client_id={}&client_secret={}&oauth_token={}&v={}'.format(user_id, CLIENT_ID, CLIENT_SECRET,
+                                                                    ACCESS_TOKEN, VERSION) # define URL
 
 # send GET request
 results = requests.get(url).json()
@@ -213,9 +215,133 @@ user_friends = json_normalize(user_data['friends']['groups'][0]['items'])
 # 1. grab prefix of photo
 # 2. grab suffix of photo
 # 3. concatenate them using the image size
-Image(url='https://igx.4sqi.net/img/user/300x300/'
-          '484542633_mK2Yum7T_7Tn9fWpndidJsmw2Hof_6T5vJBKCHPLMK5OL-U5ZiJGj51iwBstcpDLYa3Zvhvis.jpg')
+img = Image(url='https://igx.4sqi.net/img/user/300x300/'
+            '484542633_mK2Yum7T_7Tn9fWpndidJsmw2Hof_6T5vJBKCHPLMK5OL-U5ZiJGj51iwBstcpDLYa3Zvhvis.jpg')
 
 # Explore a location
 
+latitude = 40.715337
+longitude = -74.008848
+
+url = 'https://api.foursquare.com/v2/venues/' \
+      'explore?client_id={}&client_secret={}&ll={},{}&v={}&radius={}&limit={}'.format(CLIENT_ID, CLIENT_SECRET,
+                                                                                      latitude, longitude, VERSION,
+                                                                                      radius, LIMIT)
+
+results = requests.get(url).json()
+print('There are {} around Ecco restaurant.'.format(len(results['response']['groups'][0]['items'])))
+
+items = results['response']['groups'][0]['items']
+
+dataframe = json_normalize(items) # flatten JSON
+
+# filter columns
+filtered_columns = ['venue.name', 'venue.categories'] + \
+                   [col for col in dataframe.columns if col.startswith('venue.location.')] + ['venue.id']
+dataframe_filtered = dataframe.loc[:, filtered_columns]
+
+# filter the category for each row
+dataframe_filtered['venue.categories'] = dataframe_filtered.apply(get_category_type, axis=1)
+
+# clean columns
+dataframe_filtered.columns = [col.split('.')[-1] for col in dataframe_filtered.columns]
+
+print(dataframe_filtered.head(10))
+
+venues_map = folium.Map(location=[latitude, longitude], zoom_start=15) # generate map centred around Ecco
+
+
+# add Ecco as a red circle mark
+folium.CircleMarker(
+    [latitude, longitude],
+    radius=10,
+    popup='Ecco',
+    fill=True,
+    color='red',
+    fill_color='red',
+    fill_opacity=0.6
+    ).add_to(venues_map)
+
+
+# add popular spots to the map as blue circle markers
+for lat, lng, label in zip(dataframe_filtered.lat, dataframe_filtered.lng, dataframe_filtered.categories):
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        fill=True,
+        color='blue',
+        fill_color='blue',
+        fill_opacity=0.6
+        ).add_to(venues_map)
+
+# display map
+venues_map.save("maps/venues.html")
+webbrowser.open("maps/venues.html")
+
+# Explore Trending Venues
+
+# define URL
+url = 'https://api.foursquare.com/v2/venues/' \
+      'trending?client_id={}&client_secret={}&ll={},{}&v={}'.format(CLIENT_ID, CLIENT_SECRET,
+                                                                    latitude, longitude, VERSION)
+
+# send GET request and get trending venues
+results = requests.get(url).json()
+if len(results['response']['venues']) == 0:
+    trending_venues_df = 'No trending venues are available at the moment!'
+
+else:
+    trending_venues = results['response']['venues']
+    trending_venues_df = json_normalize(trending_venues)
+
+    # filter columns
+    columns_filtered = ['name', 'categories'] + ['location.distance', 'location.city', 'location.postalCode',
+                                                 'location.state', 'location.country', 'location.lat', 'location.lng']
+    trending_venues_df = trending_venues_df.loc[:, columns_filtered]
+
+    # filter the category for each row
+    trending_venues_df['categories'] = trending_venues_df.apply(get_category_type, axis=1)
+
+# display trending venues
+print(trending_venues_df)
+
+if len(results['response']['venues']) == 0:
+    venues_map = 'Cannot generate visual as no trending venues are available at the moment!'
+
+else:
+    venues_map = folium.Map(location=[latitude, longitude], zoom_start=15) # generate map centred around Ecco
+
+
+    # add Ecco as a red circle mark
+    folium.features.CircleMarker(
+        [latitude, longitude],
+        radius=10,
+        popup='Ecco',
+        fill=True,
+        color='red',
+        fill_color='red',
+        fill_opacity=0.6
+    ).add_to(venues_map)
+
+
+    # add the trending venues as blue circle markers
+    for lat, lng, label in zip(trending_venues_df['location.lat'], trending_venues_df['location.lng'],
+                               trending_venues_df['name']):
+        folium.features.CircleMarker(
+            [lat, lng],
+            radius=5,
+            poup=label,
+            fill=True,
+            color='blue',
+            fill_color='blue',
+            fill_opacity=0.6
+        ).add_to(venues_map)
+
+# display map
+try:
+    venues_map.save("maps/trending_venues.html")
+    webbrowser.open("maps/trending_venues.html")
+except:
+    print('Cannot generate visual as no trending venues are available at the moment!')
 
