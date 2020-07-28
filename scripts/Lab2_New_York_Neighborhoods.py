96c893e571067c60c4e6e1b658a7a1a9226d485c
CustodioD_Win10 <daniel.dc.custodio@gmail.com>
2020-07-27 21:29:33 -0300

    started working on the Toronto assignment

--- scripts/Lab2_New_York_Neighborhoods.py
+++ scripts/Lab2_New_York_Neighborhoods.py
@@ -2,14 +2,16 @@ import numpy as np  # library to handle data in a vectorized manner
 import json  # library to handle JSON files
 from geopy.geocoders import Nominatim  # convert an address into latitude and longitude values
 import requests  # library to handle requests
+import pandas as pd  # library for data analsysis
 from pandas import json_normalize  # transform JSON file into a pandas data frame
 import matplotlib.cm as cm
 import matplotlib.colors as colors
 from sklearn.cluster import KMeans
 import folium  # map rendering library
-import pandas as pd  # library for data analsysis
+import webbrowser
 import wget
 import os
+from data.client_config import CLIENT_ID, CLIENT_SECRET
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -24,3 +26,334 @@ with open(file_path) as json_data:
 
 neighborhoods_data = newyork_data['features']
 print(neighborhoods_data[0])
+
+# define the dataframe columns
+column_names = ['Borough', 'Neighborhood', 'Latitude', 'Longitude']
+
+# instantiate the dataframe
+neighborhoods = pd.DataFrame(columns=column_names)
+
+for data in neighborhoods_data:
+    borough = neighborhood_name = data['properties']['borough']
+    neighborhood_name = data['properties']['name']
+
+    neighborhood_latlon = data['geometry']['coordinates']
+    neighborhood_lat = neighborhood_latlon[1]
+    neighborhood_lon = neighborhood_latlon[0]
+
+    neighborhoods = neighborhoods.append({'Borough': borough,
+                                          'Neighborhood': neighborhood_name,
+                                          'Latitude': neighborhood_lat,
+                                          'Longitude': neighborhood_lon}, ignore_index=True)
+
+print(neighborhoods.head())
+
+print('The dataframe has {} boroughs and {} neighborhoods.'.format(
+        len(neighborhoods['Borough'].unique()),
+        neighborhoods.shape[0]
+    )
+)
+
+address = 'New York City, NY'
+
+geolocator = Nominatim(user_agent="ny_explorer")
+location = geolocator.geocode(address)
+latitude = location.latitude
+longitude = location.longitude
+print('The geograpical coordinate of New York City are {}, {}.'.format(latitude, longitude))
+
+# create map of New York using latitude and longitude values
+map_newyork = folium.Map(location=[latitude, longitude], zoom_start=10)
+
+# add markers to map
+for lat, lng, borough, neighborhood in zip(neighborhoods['Latitude'], neighborhoods['Longitude'],
+                                           neighborhoods['Borough'], neighborhoods['Neighborhood']):
+    label = '{}, {}'.format(neighborhood, borough)
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        color='blue',
+        fill=True,
+        fill_color='#3186cc',
+        fill_opacity=0.7,
+        parse_html=False).add_to(map_newyork)
+
+# display map
+map_newyork.save("maps/map_newyork.html")
+file_path = os.path.abspath("maps/map_newyork.html")
+webbrowser.open(file_path)
+
+# Manhattan
+
+manhattan_data = neighborhoods[neighborhoods['Borough'] == 'Manhattan'].reset_index(drop=True)
+print(manhattan_data.head())
+
+address = 'Manhattan, NY'
+
+geolocator = Nominatim(user_agent="ny_explorer")
+location = geolocator.geocode(address)
+latitude = location.latitude
+longitude = location.longitude
+print('The geograpical coordinate of Manhattan are {}, {}.'.format(latitude, longitude))
+
+# create map of Manhattan using latitude and longitude values
+map_manhattan = folium.Map(location=[latitude, longitude], zoom_start=11)
+
+# add markers to map
+for lat, lng, label in zip(manhattan_data['Latitude'], manhattan_data['Longitude'], manhattan_data['Neighborhood']):
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        color='blue',
+        fill=True,
+        fill_color='#3186cc',
+        fill_opacity=0.7,
+        parse_html=False).add_to(map_manhattan)
+
+# display map
+map_manhattan.save("maps/map_manhattan.html")
+file_path = os.path.abspath("maps/map_manhattan.html")
+webbrowser.open(file_path)
+
+VERSION = '20180605'  # Foursquare API version
+
+print('Your credentails:')
+print('CLIENT_ID: ' + CLIENT_ID)
+print('CLIENT_SECRET:' + CLIENT_SECRET)
+
+print(manhattan_data.loc[0, 'Neighborhood'])
+
+neighborhood_latitude = manhattan_data.loc[0, 'Latitude']  # neighborhood latitude value
+neighborhood_longitude = manhattan_data.loc[0, 'Longitude']  # neighborhood longitude value
+
+neighborhood_name = manhattan_data.loc[0, 'Neighborhood']  # neighborhood name
+
+print('Latitude and longitude values of {} are {}, {}.'.format(neighborhood_name,
+                                                               neighborhood_latitude,
+                                                               neighborhood_longitude))
+
+#  top 100 venues that are in Marble Hill within a radius of 500 meters
+
+LIMIT = 100 # limit of number of venues returned by Foursquare API
+radius = 500 # define radius
+# create URL
+url = 'https://api.foursquare.com/v2/venues/' \
+      'explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        VERSION,
+        neighborhood_latitude,
+        neighborhood_longitude,
+        radius,
+        LIMIT)
+
+results = requests.get(url).json()
+
+
+# function that extracts the category of the venue
+def get_category_type(row):
+    try:
+        categories_list = row['categories']
+    except:
+        categories_list = row['venue.categories']
+
+    if len(categories_list) == 0:
+        return None
+    else:
+        return categories_list[0]['name']
+
+
+venues = results['response']['groups'][0]['items']
+
+nearby_venues = json_normalize(venues)  # flatten JSON
+
+# filter columns
+filtered_columns = ['venue.name', 'venue.categories', 'venue.location.lat', 'venue.location.lng']
+nearby_venues = nearby_venues.loc[:, filtered_columns]
+
+# filter the category for each row
+nearby_venues['venue.categories'] = nearby_venues.apply(get_category_type, axis=1)
+
+# clean columns
+nearby_venues.columns = [col.split(".")[-1] for col in nearby_venues.columns]
+
+print(nearby_venues.head())
+
+print('{} venues were returned by Foursquare.'.format(nearby_venues.shape[0]))
+
+
+# Explore Neighborhoods in Manhattan
+def getNearbyVenues(names, latitudes, longitudes, radius=500):
+    venues_list = []
+    for name, lat, lng in zip(names, latitudes, longitudes):
+        print(name)
+
+        # create the API request URL
+        url = 'https://api.foursquare.com/v2/' \
+              'venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+                CLIENT_ID,
+                CLIENT_SECRET,
+                VERSION,
+                lat,
+                lng,
+                radius,
+                LIMIT)
+
+        # make the GET request
+        results = requests.get(url).json()["response"]['groups'][0]['items']
+
+        # return only relevant information for each nearby venue
+        venues_list.append([(
+            name,
+            lat,
+            lng,
+            v['venue']['name'],
+            v['venue']['location']['lat'],
+            v['venue']['location']['lng'],
+            v['venue']['categories'][0]['name']) for v in results])
+
+    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
+    nearby_venues.columns = ['Neighborhood',
+                             'Neighborhood Latitude',
+                             'Neighborhood Longitude',
+                             'Venue',
+                             'Venue Latitude',
+                             'Venue Longitude',
+                             'Venue Category']
+
+    return nearby_venues
+
+
+# Using on Manhattan
+manhattan_venues = getNearbyVenues(names=manhattan_data['Neighborhood'],
+                                   latitudes=manhattan_data['Latitude'],
+                                   longitudes=manhattan_data['Longitude']
+                                   )
+
+print(manhattan_venues.shape)
+print(manhattan_venues.head())
+print(manhattan_venues.groupby('Neighborhood').count())
+print('There are {} uniques categories.'.format(len(manhattan_venues['Venue Category'].unique())))
+
+# Analyze Each Neighborhood
+
+# one hot encoding
+manhattan_onehot = pd.get_dummies(manhattan_venues[['Venue Category']], prefix="", prefix_sep="")
+
+# add neighborhood column back to dataframe
+manhattan_onehot['Neighborhood'] = manhattan_venues['Neighborhood']
+
+# move neighborhood column to the first column
+fixed_columns = [manhattan_onehot.columns[-1]] + list(manhattan_onehot.columns[:-1])
+manhattan_onehot = manhattan_onehot[fixed_columns]
+
+print(manhattan_onehot.head())
+print(manhattan_onehot.shape)
+
+manhattan_grouped = manhattan_onehot.groupby('Neighborhood').mean().reset_index()
+print(manhattan_grouped.head())
+print(manhattan_grouped.shape)
+
+num_top_venues = 5
+
+for hood in manhattan_grouped['Neighborhood']:
+    print("----"+hood+"----")
+    temp = manhattan_grouped[manhattan_grouped['Neighborhood'] == hood].T.reset_index()
+    temp.columns = ['venue','freq']
+    temp = temp.iloc[1:]
+    temp['freq'] = temp['freq'].astype(float)
+    temp = temp.round({'freq': 2})
+    print(temp.sort_values('freq', ascending=False).reset_index(drop=True).head(num_top_venues))
+    print('\n')
+
+
+# First, let's write a function to sort the venues in descending order.
+def return_most_common_venues(row, num_top_venues):
+    row_categories = row.iloc[1:]
+    row_categories_sorted = row_categories.sort_values(ascending=False)
+
+    return row_categories_sorted.index.values[0:num_top_venues]
+
+
+num_top_venues = 10
+
+indicators = ['st', 'nd', 'rd']
+
+# create columns according to number of top venues
+columns = ['Neighborhood']
+for ind in np.arange(num_top_venues):
+    try:
+        columns.append('{}{} Most Common Venue'.format(ind+1, indicators[ind]))
+    except:
+        columns.append('{}th Most Common Venue'.format(ind+1))
+
+# create a new dataframe
+neighborhoods_venues_sorted = pd.DataFrame(columns=columns)
+neighborhoods_venues_sorted['Neighborhood'] = manhattan_grouped['Neighborhood']
+
+for ind in np.arange(manhattan_grouped.shape[0]):
+    neighborhoods_venues_sorted.iloc[ind, 1:] = return_most_common_venues(manhattan_grouped.iloc[ind, :],
+                                                                          num_top_venues)
+
+print(neighborhoods_venues_sorted.head())
+
+# Cluster Neighborhoods
+
+# set number of clusters
+kclusters = 5
+
+manhattan_grouped_clustering = manhattan_grouped.drop('Neighborhood', 1)
+
+# run k-means clustering
+kmeans = KMeans(n_clusters=kclusters, random_state=0).fit(manhattan_grouped_clustering)
+
+# check cluster labels generated for each row in the dataframe
+print(kmeans.labels_[0:10])
+
+# Let's create a new dataframe that includes the cluster as well as the top 10 venues for each neighborhood.
+# add clustering labels
+neighborhoods_venues_sorted.insert(0, 'Cluster Labels', kmeans.labels_)
+manhattan_merged = manhattan_data
+
+# merge manhattan_grouped with manhattan_data to add latitude/longitude for each neighborhood
+manhattan_merged = manhattan_merged.join(neighborhoods_venues_sorted.set_index('Neighborhood'), on='Neighborhood')
+manhattan_merged['Cluster Labels'] = manhattan_merged['Cluster Labels'].fillna(0).astype('int')
+print(manhattan_merged.head())
+
+# create map
+map_clusters = folium.Map(location=[latitude, longitude], zoom_start=11)
+
+# set color scheme for the clusters
+x = np.arange(kclusters)
+ys = [i + x + (i*x)**2 for i in range(kclusters)]
+colors_array = cm.rainbow(np.linspace(0, 1, len(ys)))
+rainbow = [colors.rgb2hex(i) for i in colors_array]
+
+# add markers to the map
+markers_colors = []
+for lat, lon, poi, cluster, most_common_venue in zip(manhattan_merged['Latitude'],
+                                                     manhattan_merged['Longitude'],
+                                                     manhattan_merged['Neighborhood'],
+                                                     manhattan_merged['Cluster Labels'],
+                                                     manhattan_merged['1st Most Common Venue']
+                                                     ):
+    label = folium.Popup(str(poi) + ' Cluster ' + str(cluster) + most_common_venue, parse_html=True)
+    folium.CircleMarker(
+        [lat, lon],
+        radius=5,
+        popup=label,
+        color=rainbow[cluster-1],
+        fill=True,
+        fill_color=rainbow[cluster-1],
+        fill_opacity=0.7).add_to(map_clusters)
+
+# display map
+map_clusters.save("maps/map_clusters.html")
+file_path = os.path.abspath("maps/map_clusters.html")
+webbrowser.open(file_path)
+
+
--- scripts/Toronto_Assignment.py
+++ scripts/Toronto_Assignment.py
@@ -1,17 +1,6 @@
 import pandas as pd
 import geocoder
 
-# # initialize your variable to None
-# lat_lng_coords = None
-#
-# # loop until you get the coordinates
-# while(lat_lng_coords is None):
-#   g = geocoder.google('{}, Toronto, Ontario'.format(postal_code))
-#   lat_lng_coords = g.latlng
-#
-# latitude = lat_lng_coords[0]
-# longitude = lat_lng_coords[1]
-
 df = pd.read_html('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M')[0]
 desired_width = 320
 pd.set_option('display.width', desired_width)
@@ -21,4 +10,30 @@ print(df.head())
 df = df[df.Borough != 'Not assigned']
 # Reset the index of the data frame
 df.index = pd.RangeIndex(len(df.index))
+print(df.head())
+
+# # with geocoder
+# for index, postal_code in zip(df.index, df['Postal Code']):
+#     # initialize your variable to None
+#     lat_lng_coords = None
+#
+#     # loop until you get the coordinates
+#     while lat_lng_coords is None:
+#         g = geocoder.google('{}, Toronto, Ontario'.format(postal_code))
+#         lat_lng_coords = g.latlng
+#
+#     latitude = lat_lng_coords[0]
+#     longitude = lat_lng_coords[1]
+#     df.loc[index, 'Latitude'] = latitude
+#     df.loc[index, 'Longitude'] = longitude
+
+# with csv file
+df_latlong = pd.read_csv('datasets/Geospatial_Coordinates.csv')
+print(df_latlong.head())
+for index, postal_code in zip(df.index, df['Postal Code']):
+
+    lat_lng_coords = df_latlong.loc[df_latlong['Postal Code'] == postal_code, ['Latitude', 'Longitude']]
+    df.loc[index, 'Latitude'] = lat_lng_coords.iloc[0, 0]
+    df.loc[index, 'Longitude'] = lat_lng_coords.iloc[0, 1]
+
 print(df.head())
